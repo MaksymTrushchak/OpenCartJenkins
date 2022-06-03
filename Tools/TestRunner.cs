3c66e4c8e491e41ceaf20facc994f5705e6dcc98
Maksym <max.trushchak@gmail.com>
2022-06-03 15:33:39 +0300

    10

--- Tools/TestRunner.cs
+++ Tools/TestRunner.cs
@@ -13,18 +13,35 @@ namespace OpencartTesting.Tools
         [SetUp]
         public void BeforeEachMethod()
         {
-            ChromeOptions options = new ChromeOptions();
-           
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+
+
+            //options.AddArgument("window-size=1920,1080");
+            //options.AddArgument("start-maximized");
+            //options.AddArgument("headless"); 
+            //options.AddArgument("disable-gpu");
+            //options.AddArgument("--no-sandbox");
+            //options.AddArgument("proxy-server='direct://'");
+            //options.AddArgument("proxy-bypass-list=*");
            
             
-            options.AddArgument("window-size=1920,1080");
-            options.AddArgument("start-maximized");
-            options.AddArgument("headless"); 
-            options.AddArgument("disable-gpu");
-            options.AddArgument("--no-sandbox");
-            options.AddArgument("proxy-server='direct://'");
-            options.AddArgument("proxy-bypass-list=*");
-            
+            chromeOptions.AddArgument("--no-sandbox");
+            chromeOptions.AddArgument("--disable-setuid-sandbox");
+
+            chromeOptions.AddArgument("--remote-debugging-port=9222"); 
+
+            chromeOptions.AddArgument("--disable-dev-shm-using");
+            chromeOptions.AddArgument("--disable-extensions");
+            chromeOptions.AddArgument("--disable-gpu");
+            chromeOptions.AddArgument("start-maximized");
+            chromeOptions.AddArgument("disable-infobars");
+            chromeOptions.AddArgument("user-data-dir=.\\cookies\\test");
+
+           
+
+
+
             driver = new ChromeDriver();
             /*
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
